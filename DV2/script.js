ffa6a6ef943cf69fd58f041e0b357b723310a913
Raveen2005 <144585263+Raveen2005@users.noreply.github.com>
2026-05-21 20:21:44 +1000

    Delete DV2 directory

--- DV2/script.js
+++
@@ -1,5 +0,0 @@
-vegaEmbed("#vis1", "charts/vis1_stops_map.json").catch(console.error);
-vegaEmbed("#vis2", "charts/vis2_patronage_line.json").catch(console.error);
-vegaEmbed("#vis3", "charts/vis3_mode_small_multiples.json").catch(console.error);
-vegaEmbed("#vis4", "charts/vis4_population_density.json").catch(console.error);
-vegaEmbed("#vis5", "charts/vis5_stop_density_heatmap.json").catch(console.error);
